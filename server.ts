6ac0b3e4d88b68494fd0cf83381e593e8f925c25
Michael Lustig <lustig@knophy.com>
2024-05-31 13:40:58 -0400

    functioning

--- server.ts
+++ server.ts
@@ -1,13 +1,43 @@
-import express from 'express';
+import express from 'express'
+import { type AnyActor, createActor } from 'xstate'
+import { gptCoordinatorMachine } from './src/gptCoordinator/gptCoordinatorMachine.ts'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+
+const actor = createActor(gptCoordinatorMachine)
+actor.start()
+
+app.get('/machineState', (req, res) => {
+  const state = actor.getSnapshot().value
+  const context = actor.getSnapshot().context
+
+  res.send({
+    state,
+    context,
+  })
+})
+
+app.post('/machineSend', (req, res) => {
+  const command = req.body.command
+  console.log(command)
+  // tood make sure right type or respond with valid types for state
+  actor.send({ type: command })
+
+  const snapshot = actor.getSnapshot()
+  const meta = snapshot.getMeta()
+  const hintsForGpt = meta.hintsForGpt
+  res.send({
+    state: snapshot.value,
+    context: snapshot.context,
+    hints: hintsForGpt,
+  })
+})
 
 app.get('/', (req, res) => {
-
-  res.send('Hello, World!');
-});
+  res.send('Hello, World!')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
