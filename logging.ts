4fe615b36b19bbc09f116a13c34b6b787786293f
Michael Lustig <lustig@knophy.com>
2024-06-08 23:28:11 +0000

    Extracted logger to its own file logging.ts

---
+++ logging.ts
@@ -0,0 +1,38 @@
+import winston from "winston";
+
+// Custom log format
+const customFormat = winston.format.printf(
+  ({ level, message, timestamp, ...meta }) => {
+    return `${timestamp} [${level}]: ${message} ${Object.keys(meta).length ? JSON.stringify(meta, null, 2) : ""}`;
+  },
+);
+
+// Setup Winston logger
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    customFormat,
+  ),
+  transports: [
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        customFormat,
+      ),
+    }),
+  );
+}
+
+export { logger };
